f9859c679e8e7934132369f2672f94f07082a264
Prasham Trivedi <prash2488@gmail.com>
2015-12-28 10:40:40 +0530

    V2.0.0-Alpha Updated example Tests remaining

--- androidexternalfilewriter/src/main/java/com/celites/androidexternalfilewriter/SAFFileWriter.java
+++ androidexternalfilewriter/src/main/java/com/celites/androidexternalfilewriter/SAFFileWriter.java
@@ -29,11 +29,20 @@ public class SAFFileWriter {
 	private DocumentFile appDirectory;
 	private DocumentFile externalCacheDirectory;
 	private DocumentFile externalParentFile;
+	private final SharedPreferences preferences;
 	private int requestCode;
 	private static final String canNotCreateDirectory = "Can not create directory: ";
 	private static final String canNotWriteFile = "Can not write file: ";
 
 
+	/**
+	 * Inits new SAFFileWriter object, it will first check whether we already have a parent directory with proper uri access or not.
+	 *
+	 * @param activity:
+	 * 		Activity for context and starting request for OPEN_DOCUMENT_TREE
+	 * @param requestCode:
+	 * 		Request code to listen to OPEN_DOCUMENT_TREE
+	 */
 	public SAFFileWriter(Activity activity, int requestCode) {
 		this.activity = activity;
 		this.requestCode = requestCode;
@@ -48,7 +57,7 @@ public class SAFFileWriter {
 		} else {
 			externalCacheDirectory = DocumentFile.fromFile(dirs[0]);
 		}
-		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(activity);
+		preferences = PreferenceManager.getDefaultSharedPreferences(activity);
 		String externalDirUrl = preferences.getString(PARENT_URI_KEY, "");
 		if (ConstantMethods.isEmptyString(externalDirUrl)) {
 			Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
@@ -56,15 +65,34 @@ public class SAFFileWriter {
 		}
 	}
 
+	/**
+	 * Creates subdirectory in parent directory
+	 *
+	 * @param parentDirectory
+	 * 		: Parent directory where directory with "directoryName" should be created
+	 * @param displayName
+	 * 		name of subdirectory
+	 *
+	 * @return File object of created subdirectory
+	 *
+	 * @throws ExternalFileWriterException
+	 * 		if external storage is not available
+	 */
 	public DocumentFile createSubDirectory(String displayName, DocumentFile parentDirectory) {
-		return parentDirectory.createDirectory(displayName);
+		getAppDirectory();
+		if (isDirectoryExists(displayName, parentDirectory)) {
+
+			return parentDirectory.createDirectory(displayName);
+		} else {
+			return parentDirectory.findFile(displayName);
+		}
 	}
 
-	public DocumentFile createSubdirectory(String directoryName, boolean inCache) {
-		DocumentFile appDirectory = getAppDirectory(inCache);
-		return appDirectory.createDirectory(directoryName);
-	}
-
+	/**
+	 * Get created app directory
+	 *
+	 * @return File object of created AppDirectory
+	 */
 	public DocumentFile getAppDirectory() {
 		if (appDirectory == null) {
 			createAppDirectory();
@@ -72,28 +100,62 @@ public class SAFFileWriter {
 		return appDirectory;
 	}
 
+	/**
+	 * Check whether directory with given name exists in parentDirectory or not.
+	 *
+	 * @param directoryName
+	 * 		: Name of the directory to check.
+	 * @param parentDirectory
+	 * 		: Parent directory where directory with "directoryName" should be present
+	 *
+	 * @return true if a directory with "directoryName" exists, false otherwise
+	 */
+	public boolean isDirectoryExists(String displayName, DocumentFile parentDirectory) {
+		DocumentFile file = parentDirectory.findFile(displayName);
+		return file != null && file.isDirectory();
+	}
+
+	/** Creates app directory */
 	private void createAppDirectory() {
 		String directoryName = activity.getString(activity.getApplicationInfo().labelRes);
 		appDirectory = externalParentFile.createDirectory(directoryName);
 		appCacheDirectory = externalCacheDirectory.createDirectory(directoryName);
 	}
 
-	public void handleResult(int requestCode, int resultCode, Intent data) {
-		if (resultCode == Activity.RESULT_OK) {
-			if (requestCode == this.requestCode) {
-				Uri treeUri = data.getData();
-				externalParentFile = DocumentFile.fromTreeUri(activity, treeUri);
+	/**
+	 * Creates subdirectory in application directory
+	 *
+	 * @param directoryName
+	 * 		name of subdirectory
+	 *
+	 * @return File object of created subdirectory
+	 *
+	 * @throws ExternalFileWriterException
+	 * 		if external storage is not available
+	 */
+	public DocumentFile createSubdirectory(String directoryName, boolean inCache) {
+		getAppDirectory();
+		DocumentFile appDirectory = getAppDirectory(inCache);
+		if (!isDirectoryExists(directoryName, inCache)) {
 
-
-			}
+			return appDirectory.createDirectory(directoryName);
+		} else {
+			return appDirectory.findFile(directoryName);
 		}
 	}
 
-	public boolean isDirectoryExists(String displayName, DocumentFile parentDirectory) {
-		DocumentFile file = parentDirectory.findFile(displayName);
-		return file != null && file.isDirectory();
+	public DocumentFile getAppDirectory(boolean inCache) {
+		return (inCache) ? this.appCacheDirectory : this.appDirectory;
 	}
 
+	/**
+	 * Checks whether directory with given name exists in AppDirectory
+	 *
+	 * @param directoryName
+	 * 		: Name of the directory to check.
+	 *
+	 * @return true if a directory with "directoryName" exists, false otherwise
+	 */
 	public boolean isDirectoryExists(String displayName, boolean inCache) {
 		DocumentFile file = getDocumentFile(displayName, inCache);
 		return file != null && file.isDirectory();
@@ -104,26 +166,72 @@ public class SAFFileWriter {
 		return appDirectory.findFile(displayName);
 	}
 
+	public void handleResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == Activity.RESULT_OK) {
+			if (requestCode == this.requestCode) {
+				Uri treeUri = data.getData();
+				externalParentFile = DocumentFile.fromTreeUri(activity, treeUri);
+				preferences.edit().putString(PARENT_URI_KEY, String.valueOf(externalParentFile.getUri()));
+				getAppDirectory();
+
+			}
+		}
+	}
+
+	/**
+	 * Check whether file with given name exists in parentDirectory or not.
+	 *
+	 * @param fileName
+	 * 		: Name of the file to check.
+	 * @param parentDirectory
+	 * 		: Parent directory where directory with "fileName" should be present
+	 *
+	 * @return true if a file  with "fileName" exists, false otherwise
+	 */
 	public boolean isFileExists(String displayName, boolean inCache) {
 		DocumentFile file = getDocumentFile(displayName, inCache);
 		return file != null && file.isFile();
 	}
 
-	public boolean isFileExists(String displayName, DocumentFile parentDirectory) {
-		DocumentFile file = parentDirectory.findFile(displayName);
-		return file != null && file.isFile();
-	}
-
 	public void writeDataToFile(String fileName, String mimeType, byte[] data, boolean inCache) throws FileNotFoundException {
+		getAppDirectory();
 		DocumentFile appDir = getAppDirectory(inCache);
 		writeDataToFile(appDir, fileName, data, mimeType);
 	}
 
+	/**
+	 * Writes data to the file. The file will be created in the directory name same as app.
+	 *
+	 * @param fileName
+	 * 		name of the file
+	 * @param data
+	 * 		data to write
+	 *
+	 * @throws ExternalFileWriterException
+	 * 		if external storage is not available or free space is less than size of the data
+	 */
 	public void writeDataToFile(DocumentFile parent, String fileName, byte[] data, String mimeType) throws FileNotFoundException {
 		DocumentFile file = createFile(fileName, parent, mimeType);
 		writeDataToFile(file, data);
 	}
 
+	private DocumentFile createFile(String fileName, DocumentFile parent, String mimeType) {
+		if (!isFileExists(fileName, parent)) {
+
+			return parent.createFile(mimeType, fileName);
+		} else {
+			return parent.findFile(fileName);
+		}
+	}
+
+	/**
+	 * Write byte array to file. Will show error if given file is a directory.
+	 *
+	 * @param file
+	 * 		: File where data is to be written.
+	 * @param data
+	 * 		byte array which you want to write a file. If size of this is greater than size available, it will show error.
+	 */
 	private void writeDataToFile(DocumentFile file, byte[] data) throws FileNotFoundException {
 		ParcelFileDescriptor fileDescriptor = activity.getContentResolver().openFileDescriptor(file.getUri(), "w");
 		FileOutputStream out = null;
@@ -138,11 +246,37 @@ public class SAFFileWriter {
 		}
 	}
 
+	/**
+	 * Checks whether file with given name exists in AppDirectory
+	 *
+	 * @param fileName
+	 * 		: Name of the file to check.
+	 *
+	 * @return true if a file with "directoryName" exists, false otherwise
+	 */
+	public boolean isFileExists(String displayName, DocumentFile parentDirectory) {
+		DocumentFile file = parentDirectory.findFile(displayName);
+		return file != null && file.isFile();
+	}
+
 	public void writeDataToFile(String fileName, String mimeType, String data, boolean inCache) throws FileNotFoundException {
 		DocumentFile appDir = getAppDirectory(inCache);
 		writeDataToFile(appDir, fileName, data, mimeType);
 	}
 
+	/**
+	 * Write data in file of a parent directory
+	 *
+	 * @param parent
+	 * 		parent directory
+	 * @param fileName
+	 * 		desired filename
+	 * @param data
+	 * 		data
+	 *
+	 * @throws ExternalFileWriterException
+	 * 		if external storage is not available or free space is less than size of the data
+	 */
 	public void writeDataToFile(DocumentFile parent, String fileName, String data, String mimeType) throws FileNotFoundException {
 		DocumentFile file = createFile(fileName, parent, mimeType);
 		writeDataToFile(file, data);
@@ -191,14 +325,6 @@ public class SAFFileWriter {
 
 	private DocumentFile createFile(String fileName, boolean inCache, String mimeType) {
 		return createFile(fileName, getAppDirectory(inCache), mimeType);
-	}
-
-	private DocumentFile createFile(String fileName, DocumentFile parent, String mimeType) {
-		return parent.createFile(mimeType, fileName);
-	}
-
-	public DocumentFile getAppDirectory(boolean inCache) {
-		return (inCache) ? this.appCacheDirectory : this.appDirectory;
 	}
 
 }
--- app/src/main/java/com/celites/appexternalfilewriter/MainActivity.java
+++ app/src/main/java/com/celites/appexternalfilewriter/MainActivity.java
@@ -1,29 +1,38 @@
 package com.celites.appexternalfilewriter;
 
 import android.content.Intent;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
+import android.support.v4.provider.DocumentFile;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ToggleButton;
 import com.celites.androidexternalfilewriter.AppExternalFileWriter;
-import com.celites.androidexternalfilewriter.ExternalFileWriterException;
+import com.celites.androidexternalfilewriter.SAFFileWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class MainActivity
 		extends AppCompatActivity {
 
 	private ToggleButton inCache;
+	private SAFFileWriter safFileWriter;
+	private DocumentFile testDirectory;
 	private File testFolder;
 	private AppExternalFileWriter writer;
+	private static final int REQUEST_FILE_ACCESS = 2488;
 
 	public void createSubFolderInAppFolder(View v) {
-		try {
-			testFolder = writer.createSubDirectory("test", inCache.isChecked());
-		} catch (ExternalFileWriterException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
+		if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+			testDirectory = safFileWriter.createSubdirectory("test", inCache.isChecked());
+		} else {
+			try {
+				testFolder = writer.createSubDirectory("test", inCache.isChecked());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -35,50 +44,74 @@ public class MainActivity
 	}
 
 	public void writeDataIntoSubFolder(View v) {
-
-		try {
-			if (testFolder == null) {
-				testFolder = writer.createSubDirectory("test", inCache.isChecked());
+		if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+			testDirectory = safFileWriter.createSubdirectory("test", inCache.isChecked());
+			try {
+				safFileWriter.writeDataToFile(testDirectory, "testFile", getString(R.string.loremipsum), "file/txt");
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
 			}
-			writer.writeDataToFile(testFolder, "testFile", getString(R.string.loremipsum));
-		} catch (ExternalFileWriterException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
+		} else {
+			try {
+				if (testFolder == null) {
+					testFolder = writer.createSubDirectory("test", inCache.isChecked());
+				}
+				writer.writeDataToFile(testFolder, "testFile", getString(R.string.loremipsum));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
 	public void writeDataIntoTestFile(View v) {
-		try {
-			writer.writeDataToFile("testFile", getString(R.string.loremipsum), inCache.isChecked());
-		} catch (ExternalFileWriterException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
+		if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+			try {
+				safFileWriter.writeDataToFile("testFile", "file/txt", getString(R.string.loremipsum), inCache.isChecked());
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				writer.writeDataToFile("testFile", getString(R.string.loremipsum), inCache.isChecked());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
 	public void writeTimeStampedFile(View v) {
-		try {
-			writer.writeDataToTimeStampedFile("txt", getString(R.string.loremipsum), inCache.isChecked());
-		} catch (ExternalFileWriterException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
+		if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+			try {
+				safFileWriter.writeDataToTimeStampedFile("file/txt", getString(R.string.loremipsum), "txt", inCache.isChecked());
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				writer.writeDataToTimeStampedFile("txt", getString(R.string.loremipsum), inCache.isChecked());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		writer.handleResult(requestCode, resultCode, data);
+		//		writer.handleResult(requestCode, resultCode, data);
+		if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+			safFileWriter.handleResult(requestCode, resultCode, data);
+		}
 	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		writer = new AppExternalFileWriter(this, true);
+		writer = new AppExternalFileWriter(this);
+		if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+			safFileWriter = new SAFFileWriter(this, REQUEST_FILE_ACCESS);
+		}
 		inCache = (ToggleButton) findViewById(R.id.toggleButton);
 	}
 }
